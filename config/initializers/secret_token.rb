c0a69c8f415110b1ceec0a6209090b69689631ff
Andrew Stopani <astopani@tammantech.com>
2012-09-17 15:43:55 -0400

    fiurst commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeployTest::Application.config.secret_token = '259f5d80c1802f41d94d7ccd872a86f4bb9905b500aa54d7fd6d1387be4ed12f569c8985055add624fc3cccb65a676c93955eb299d1574883a0a78558de05505'
